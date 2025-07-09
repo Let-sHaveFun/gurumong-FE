e97e21e4e9291c1d1fbd81650496345c7f777cb4
Sumin <tnalsdldkr2@naver.com>
2025-07-09 22:11:21 +0900

    chore: axios 인스턴스 생성 및 기본 설정 타입 정의

---
+++ src/shared/lib/axios.ts
@@ -0,0 +1,13 @@
+import axios, { type CreateAxiosDefaults } from 'axios'
+
+export const API_BASE_URL = import.meta.env.VITE_BASE_URL
+
+const baseConfig: CreateAxiosDefaults = {
+  baseURL: API_BASE_URL,
+  headers: {
+    'Content-Type': 'application/json', 
+  },
+  withCredentials: true,
+}
+
+export const apiClient = axios.create(baseConfig)
