9cf72894e4679b0e312779e2e4ff3e3513243071
Sumin <tnalsdldkr2@naver.com>
2025-07-10 11:44:14 +0900

    feat: 홈페이지 height 수정

--- src/shared/ui/MobileLayout.tsx
+++ src/shared/ui/MobileLayout.tsx
@@ -4,7 +4,7 @@ import { Nav } from './Nav';
 export function MobileLayout() {
   return (
     <div className="flex justify-center min-h-screen bg-gray-50">
-      <div className="w-full bg-white shadow-lg max-w-[393px] min-h-[100vh]">
+      <div className="w-full bg-white shadow-lg max-w-[393px] min-h-screen flex flex-col">
         <Outlet />
         <Nav />
       </div>
