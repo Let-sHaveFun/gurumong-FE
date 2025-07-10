98ccb1716c4d3c7b135f0140ff92c07820912c9d
KIMSEUNGGYU <45627868+KIMSEUNGGYU@users.noreply.github.com>
2025-07-11 01:16:47 +0900

    feat: Enhance Nav component with MyPage dialog functionality
    - Introduced a new MyPage section in the Nav component, allowing users to open a dialog for notifications.
    - Implemented state management for dialog visibility using useState.
    - Added a Dialog component to display a notification message indicating that the MyPage feature is still under development.
    - Commented out the previous MyPage link to streamline navigation while the feature is being finalized.
    - These changes aim to improve user engagement by providing feedback on upcoming features.

--- src/shared/ui/Nav.tsx
+++ src/shared/ui/Nav.tsx
@@ -1,13 +1,16 @@
 import { Link, useLocation } from 'react-router-dom';
 import { LocationIcon, ExamIcon, UserIcon } from '@vapor-ui/icons';
+import { Dialog } from '@vapor-ui/core';
+import { useState } from 'react';
 
 export function Nav() {
   const location = useLocation();
+  const [isMyPageOpen, setIsMyPageOpen] = useState(false);
 
   const navItems = [
     { to: '/', icon: LocationIcon, label: '지도' },
     { to: '/story-fragments', icon: ExamIcon, label: '이야기조각' },
-    { to: '/mypage', icon: UserIcon, label: '마이페이지' },
+    // { to: '/mypage', icon: UserIcon, label: '마이페이지' },
   ];
 
   return (
@@ -30,7 +33,35 @@ export function Nav() {
             </Link>
           );
         })}
+
+        <div className="flex flex-col justify-center items-center cursor-pointer" onClick={() => setIsMyPageOpen(true)}>
+          <UserIcon size={32} color={'var(--vapor-color-gray-400)'} />
+          <span
+            style={{
+              // color: isActive ? 'var(--vapor-color-blue-400)' : 'var(--vapor-color-gray-400)',
+              color: 'var(--vapor-color-gray-400)',
+              fontSize: 'var(--vapor-typography-fontSize-050)',
+            }}
+          >
+            마이페이지
+          </span>
+        </div>
       </nav>
+
+      <Dialog.Root open={isMyPageOpen} onOpenChange={setIsMyPageOpen}>
+        <Dialog.Overlay className="" />
+        <Dialog.CombinedContent className="w-[393px] h-[30%] p-10 border-none justify-center gap-2 z-[10] ">
+          <Dialog.Header>
+            <Dialog.Title>알림</Dialog.Title>
+          </Dialog.Header>
+          <Dialog.Body className="z-[10] flex flex-col justify-center items-center">
+            <span className="text-black z-[10]">Oops! 아직 준비중이에요. 🚀</span>
+          </Dialog.Body>
+          {/* <Dialog.Footer style={{ marginLeft: 'auto' }}>
+            <button style={{ padding: '0.5rem 1rem' }}>확인</button>
+          </Dialog.Footer> */}
+        </Dialog.CombinedContent>
+      </Dialog.Root>
     </footer>
   );
 }
