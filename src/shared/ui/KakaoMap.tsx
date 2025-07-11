e9c821e6768c6cde34a5cbd11405fe52130d65f7
KIMSEUNGGYU <45627868+KIMSEUNGGYU@users.noreply.github.com>
2025-07-11 09:02:55 +0900

    fix: Update heritage property names for consistency in KakaoMap and ResultMap components
    - Changed property names from `external_id` to `externalId` and `imgpath` to `ImagePath` in both `KakaoMap.tsx` and `ResultMap.tsx` for consistency with the updated data structure.
    - This update aims to enhance code clarity and ensure proper alignment with the heritage data model.

--- src/shared/ui/KakaoMap.tsx
+++ src/shared/ui/KakaoMap.tsx
@@ -115,20 +115,19 @@ export const KakaoMap = () => {
 
         {heritages.map((heritage) => (
           <MapMarker
-            key={heritage['external_id']}
+            key={heritage['externalId']}
             position={{ lat: heritage.latitude, lng: heritage.longitude }}
             onClick={() => {
-              setActiveHeritageId(heritage['external_id']);
+              setActiveHeritageId(heritage['externalId']);
               setSelectedHeritage(heritage);
               setIsDrawerOpen(true);
               setActiveButton('list');
             }}
             image={{
-              src: heritage['external_id'] === activeHeritageId ? '/active-spot-marker.svg' : '/spot-marker.svg',
-              size:
-                heritage['external_id'] === activeHeritageId ? { width: 40, height: 40 } : { width: 32, height: 32 },
+              src: heritage['externalId'] === activeHeritageId ? '/active-spot-marker.svg' : '/spot-marker.svg',
+              size: heritage['externalId'] === activeHeritageId ? { width: 40, height: 40 } : { width: 32, height: 32 },
               options:
-                heritage['external_id'] === activeHeritageId
+                heritage['externalId'] === activeHeritageId
                   ? { offset: { x: 20, y: 40 } }
                   : { offset: { x: 16, y: 32 } },
             }}
@@ -175,9 +174,9 @@ export const KakaoMap = () => {
                 <div className="h-full min-h-[300px] overflow-y-auto">
                   {heritages.map((heritage) => (
                     <SpotCard
-                      imageUrl={heritage['imgpath']}
-                      key={heritage['external_id']}
-                      id={heritage['external_id']}
+                      imageUrl={heritage['ImagePath']}
+                      key={heritage['externalId']}
+                      id={heritage['externalId']}
                       title={heritage.name}
                       address={heritage.address}
                       distance={heritage.distance}
