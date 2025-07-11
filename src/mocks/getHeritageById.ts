90f7f305471dabe6e7cead80664f0925fd9719b8
KIMSEUNGGYU <45627868+KIMSEUNGGYU@users.noreply.github.com>
2025-07-11 09:32:37 +0900

    fix: Update API endpoint in getHeritageById for accurate data retrieval
    - Changed the API endpoint in `getHeritageById.ts` to fetch heritage details from the new URL: `https://dormung.goorm.training/api/tour-spots/detail?contentId=${id}`.
    - This update ensures accurate retrieval of heritage data based on the provided ID, aligning with the current API structure.

--- src/mocks/getHeritageById.ts
+++ src/mocks/getHeritageById.ts
@@ -1,7 +1,9 @@
 import { type Heritage } from '@/pages/home/api/getNearbyHeritages.mock';
 
+// https://dormung.goorm.training/api/tour-spots/location
+// https://dormung.goorm.training/api/tour-spots/detail?contentId=CONT_000000000500150
 export const getHeritageById = async (id: string): Promise<Heritage | null> => {
-  const response = await fetch(`https://api.example.com/heritage/${id}`);
+  const response = await fetch(`https://dormung.goorm.training/api/tour-spots/detail?contentId=${id}`);
   const data = await response.json();
   return data ?? null;
 };
