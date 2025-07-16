8607b569e8a624fa6e56930ca31ee61bb532ffaa
KIMSEUNGGYU <45627868+KIMSEUNGGYU@users.noreply.github.com>
2025-07-16 15:57:27 +0900

    refactor: Clean up fetchHeritageById function by removing commented-out code
    - Removed unused commented-out code in the `fetchHeritageById` function to enhance code readability and maintainability.
    - The function now directly utilizes the `apiClient` for fetching heritage details, ensuring a cleaner implementation.

--- src/entities/heritage/api/index.ts
+++ src/entities/heritage/api/index.ts
@@ -3,11 +3,6 @@ import { apiClient } from '@/shared/lib/axios';
 import type { Heritage } from '../model/heritage.type';
 
 export const fetchHeritageById = async (id: string, lat: number, lng: number) => {
-  // const response = await fetch(
-  //   `https://dormung.goorm.training/api/tour-spots/detail?contentId=${id}&latitude=${lat}&longitude=${lng}`,
-  // );
-  // const data = await response.json();
-  // return data ?? null;
   const response = await apiClient.get<Heritage>(`/tour-spots/detail?contentId=${id}&latitude=${lat}&longitude=${lng}`);
   return response.data;
 };
