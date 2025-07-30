6df1bb5996390302f6254828a0c0f7e6b6fd46ae
KIMSEUNGGYU <45627868+KIMSEUNGGYU@users.noreply.github.com>
2025-07-30 10:43:49 +0900

    feat: api url 변경

--- src/shared/lib/axios.ts
+++ src/shared/lib/axios.ts
@@ -1,7 +1,6 @@
 import axios, { type CreateAxiosDefaults } from 'axios';
 
-export const API_BASE_URL = 'https://dormung.goorm.training/api';
-// import.meta.env.VITE_BASE_URL;
+export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 const baseConfig: CreateAxiosDefaults = {
   baseURL: API_BASE_URL,
