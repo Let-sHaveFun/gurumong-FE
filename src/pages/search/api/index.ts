9a7e239cf6d9d9fe938ccf2d5c5c098b1e5ac637
KIMSEUNGGYU <45627868+KIMSEUNGGYU@users.noreply.github.com>
2025-08-10 12:12:00 +0900

    feat: 기존 하드 코딩된 api 주소를 api client 로 변경

--- src/pages/search/api/index.ts
+++ src/pages/search/api/index.ts
@@ -1,8 +1,7 @@
+import { apiClient } from '@/shared/lib/axios';
 // "https://dormung.goorm.training/api/tour-spots/search?keyword=%EC%84%B1%EC%82%B0&latitude=33.462147&longitude=126.936424"
 
 export const search = async (query: string) => {
-  // const response = await fetch(`https://api.example.com/search?query=${query}`);
-  const response = await fetch(`https://dormung.goorm.training/api/tour-spots/search?keyword=${query}`);
-  const data = await response.json();
-  return data;
+  const response = await apiClient.get(`/tour-spots/search?keyword=${query}`);
+  return response.data;
 };
