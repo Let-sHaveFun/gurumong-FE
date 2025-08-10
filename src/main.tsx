2f840fba8bb3520a7123a9c434330f55c73f75e9
KIMSEUNGGYU <45627868+KIMSEUNGGYU@users.noreply.github.com>
2025-08-10 11:36:28 +0900

    feat: msw 환경 실행 제거

--- src/main.tsx
+++ src/main.tsx
@@ -12,7 +12,7 @@ async function enableMocking() {
 
   // `worker.start()` returns a Promise that resolves
   // once the Service Worker is up and ready to intercept requests.
-  return worker.start();
+  // return worker.start();
 }
 
 enableMocking().then(() => {
